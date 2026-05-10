74bc3ccd234b0eff3da0362cb3ced5919ec747f6
MykhailoZinenko <mzinenko.work@gmail.com>
2026-05-10 19:40:09 +0200

    fix: replace custom EnsureCheckoutAuthenticated with built-in auth middleware

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,6 @@ use App\Http\Controllers\CheckoutController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
-use App\Http\Middleware\EnsureCheckoutAuthenticated;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomeController::class)->name('home');
@@ -25,7 +24,7 @@ Route::patch('cart/options', [CartController::class, 'updateOptions'])->name('ca
 Route::patch('cart/items/{item}', [CartController::class, 'update'])->name('cart.update');
 Route::delete('cart/items/{item}', [CartController::class, 'remove'])->name('cart.remove');
 
-Route::middleware(EnsureCheckoutAuthenticated::class)->group(function () {
+Route::middleware('auth')->group(function () {
     Route::get('checkout', [CheckoutController::class, 'show'])->name('checkout.show');
     Route::post('checkout', [CheckoutController::class, 'store'])->name('checkout.store');
     Route::get('checkout/success/{order}', [CheckoutController::class, 'success'])->name('checkout.success');
