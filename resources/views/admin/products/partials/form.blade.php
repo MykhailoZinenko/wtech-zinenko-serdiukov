c2f129eb69a28b768c3eda3716d480f8e05ef61a
MykhailoZinenko <mzinenko.work@gmail.com>
2026-05-10 23:23:22 +0200

    fix: save scroll position before programmatic image delete submit

--- resources/views/admin/products/partials/form.blade.php
+++ resources/views/admin/products/partials/form.blade.php
@@ -59,7 +59,7 @@
                                     <input type="radio" name="primary_image_id" value="{{ $image->id }}" @checked(old('primary_image_id', $product->primaryImage?->id) == $image->id) />
                                     Primary
                                 </label>
-                                <button type="button" class="img-existing__rm" onclick="document.getElementById('delete-image-{{ $image->id }}').submit()"><i class="bi bi-x"></i></button>
+                                <button type="button" class="img-existing__rm" onclick="sessionStorage.setItem('scrollY',String(window.scrollY));sessionStorage.setItem('scrollUrl',location.pathname+location.search);document.getElementById('delete-image-{{ $image->id }}').submit()"><i class="bi bi-x"></i></button>
                             </div>
                         @endforeach
                     @endif
